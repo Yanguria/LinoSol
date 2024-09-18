0e5169224b60fbbd27507d3898a984402a9647b8
yanguria <marior.deang@gmail.com>
2024-09-18 23:58:15 +0200

    First Commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "LinoSol.h"
+#include <vector>
+int main() {
+
+    int n = 100; // Cells number
+    double L = 29; // Line Length
+    std::vector<double> domain = mesh_1d(L,n);
+
+
+}
+
